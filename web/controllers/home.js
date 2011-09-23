2e543eda3255aaf3258bb323823422de239057d7
Karl Seguin <karlseguin@gmail.com>
2011-09-23 15:28:39 +0800

    about page

--- web/controllers/home.js
+++ web/controllers/home.js
@@ -16,11 +16,7 @@ module.exports = function(app){
   });
   
   app.get('/about', function(req, res){
-    User.loadFromCredentials(req.body.username, req.body.password, function( user){
-      if (user == null) { res.render('login', {error: 'invalid username or password, please try again'}); return; }
-      signin(req, user, res)
-      res.redirect('/stats?l=1');
-    });
+    res.render('about');
   });
   
   app.get('/logout', sessions({secret: app.set('secret')}), function(req, res){
